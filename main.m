5f00acd1be274d3988570e77bb8712b42063393c
GURINDER SINGH <47207827+GurinderSingh97@users.noreply.github.com>
2019-04-04 16:32:18 +0530

    Add files via upload

--- main.m
+++ main.m
@@ -10,41 +10,40 @@ disp ("Design of Square Footing")
 disp("\n")
 %Compute the Area of the Footing
 Load_on_Column = Load;
-printf("Load_on_Column = %d KN \n",Load_on_Column)
+printf("Load_on_Column = %d N \n",Load_on_Column)
 
 Assume_the_Self_Weight_of_Footing = (Self_Weight);
 printf("Assume_the_Self_Weight_of_Footing = %d %% \n",Assume_the_Self_Weight_of_Footing)
 
 Load_inc = (Self_Weight/100)*Load;
 Self_Weight_of_Footing = Load_inc;
-printf("Self_Weight_of_Footing = %d KN \n",Self_Weight_of_Footing)
+printf("Self_Weight_of_Footing = %d N \n",Self_Weight_of_Footing)
 
 Vertical_Load_on_Column = Load+Load_inc;
-printf("Vertical_Load_on_Column = %d KN \n",Vertical_Load_on_Column)
+printf("Vertical_Load_on_Column = %d N \n",Vertical_Load_on_Column)
 
 disp("\n")
 Required_Area_of_Footing = (Vertical_Load_on_Column/Soil_Pressure);
-printf("Required_Area_of_Footing = %d m^2 \n",Required_Area_of_Footing)
-
-Provided_Area_of_Footing = round(Vertical_Load_on_Column/Soil_Pressure);
-printf("Provided_Area_of_Footing = %d m^2 \n",Provided_Area_of_Footing)
+printf("Required_Area_of_Footing = %d mm^2 \n",Required_Area_of_Footing)
 
 % Side of Rectangular Footing
+Side_of_Footing = round((sqrt(Required_Area_of_Footing))/1000)*1000;
+printf("Side_of_Footing = %d mm \n",Side_of_Footing)
 
-Side_of_Footing = round(sqrt(Provided_Area_of_Footing)/0.1)*0.1;
-printf("Side_of_Footing = %d m^2 \n",Side_of_Footing)
+Provided_Area_of_Footing = Side_of_Footing*Side_of_Footing;
+printf("Provided_Area_of_Footing = %d m^2 \n",Provided_Area_of_Footing)
 
 % Net Upward pressure
-Net_Upward_Pressure = (Load)/(Side_of_Footing*Side_of_Footing);
-printf("Net_Upward_Pressure = %d KN/m^2 \n",Net_Upward_Pressure)
+Net_Upward_Pressure = (1.5*Load)/(Side_of_Footing*Side_of_Footing);
+printf("Net_Upward_Pressure = %d N/mm^2 \n",Net_Upward_Pressure)
 
 % Depth on the basis of moment
 disp("\n")
 disp("Depth on the basis of Bending Compression")
 
 
-M = Net_Upward_Pressure*(Side_of_Footing/8)*(Side_of_Footing-Side_of_Column)*(Side_of_Footing-Side_of_Column)*1000000;
-printf("Moment at the Face of the Column = %d Nmm \n",M)
+M = Net_Upward_Pressure*(Side_of_Footing/8)*(Side_of_Footing-Side_of_Column)*(Side_of_Footing-Side_of_Column);
+printf("Moment_at_the_Face_of_the_Column = %d Nmm \n",M)
 
 Factored_Moment_at_the_Face_of_the_Column = 1.5*M;
 printf("Factored_Moment_at_the_Face_of_the_Column = %d Nmm \n",Factored_Moment_at_the_Face_of_the_Column)
@@ -52,78 +51,62 @@ printf("Factored_Moment_at_the_Face_of_the_Column = %d Nmm \n",Factored_Moment_a
 xu_max_by_d = (700)/(1100+0.87*Fy);
 Ru = (0.36*Fck*xu_max_by_d*(1-0.416*xu_max_by_d));
 
-Effective_Depth_on_the_basis_of_Moment= round(sqrt((Factored_Moment_at_the_Face_of_the_Column)/(Ru*Side_of_Footing*1000))/10)*10;
+Effective_Depth_on_the_basis_of_Moment= round(sqrt((Factored_Moment_at_the_Face_of_the_Column)/(Ru*Side_of_Footing))/10)*10;
 printf("Effective_Depth_on_the_basis_of_Moment = %d mm \n",Effective_Depth_on_the_basis_of_Moment)
 
 Overall_Depth_on_the_basis_of_Moment = Effective_Depth_on_the_basis_of_Moment+Clear_Cover;
 printf("Overall_Depth_on_the_basis_of_Moment  = %d mm \n",Overall_Depth_on_the_basis_of_Moment)
 
 disp("\n")
-disp("Depth on the basis of One Way Shear")
-Permissible_Shear_Stress1 = interp2(tables,tables,tables,Fck,pt);
+disp("Depth on the basis of One Way Shear:-")
+if(Fck>40)
+fck=40;
+elseif(Fck<40)
+fck=Fck;
+endif
+Permissible_Shear_Stress1 = interp2(tables,tables,tables,fck,pt);
+if(Overall_Depth_on_the_basis_of_Moment>300)
+Overall_Depth_on_the_basis_of_Moment=300;
+endif
 k = interp1(table(:,1),table(:,2),Overall_Depth_on_the_basis_of_Moment);
 
 Permissible_Shear_Stress = k*Permissible_Shear_Stress1;
 printf("Permissible_Shear_Stress = %d N/mm^2 \n",Permissible_Shear_Stress) 
 
-Effective_Depth_on_the_basis_of_One_Way_Shear = round((((0.75*Net_Upward_Pressure*Side_of_Footing-0.75*Net_Upward_Pressure*Side_of_Column))/(Permissible_Shear_Stress+0.0015*Net_Upward_Pressure))/10)*10;
-printf("Effective_Depth_on_the_basis_of_One_Way_Shear = %d mm \n",Effective_Depth_on_the_basis_of_One_Way_Shear) 
-Overall_Depth_One_Way_Shear = Effective_Depth_on_the_basis_of_One_Way_Shear+Clear_Cover;
-printf("Overall_Depth_on_the_basis_of_One_Way_Shear = %d mm \n",Overall_Depth_One_Way_Shear)
+L_bey_col = (Side_of_Footing-Side_of_Column)/2;
+d_one_way_shear = round((Net_Upward_Pressure*L_bey_col)/(Net_Upward_Pressure+Permissible_Shear_Stress));
+printf("Eff_depth_one_way_shear = %d N/mm^2 \n",d_one_way_shear)
 
 disp("\n")
-if(Effective_Depth_on_the_basis_of_Moment>Effective_Depth_on_the_basis_of_One_Way_Shear)
-Effective_Depth_of_the_Footing = Effective_Depth_on_the_basis_of_Moment;
+disp("Depth on the basis of Two Way Shear:-")
+Width = Side_of_Column+d_one_way_shear;
+Shear_Force_around_the_section = (Net_Upward_Pressure*(Side_of_Footing*Side_of_Footing-(Width*Width)));
+printf("Shear_Force_around_the_section = %d N \n",Shear_Force_around_the_section)
 
-Overall_Depth_of_the_Footing = Effective_Depth_of_the_Footing+Clear_Cover_for_Footing;
-printf("Overall_Depth_of_the_Footing = %d mm \n",Overall_Depth_of_the_Footing)
-
-Effective_Depth_of_Footing_in_one_direction = Overall_Depth_of_the_Footing-Clear_Cover;
-printf("Effective_Depth_of_Footing_in_one_direction = %d mm \n",Effective_Depth_of_Footing_in_one_direction)
-
-Effective_Depth_of_Footing_in_other_direction = Effective_Depth_of_Footing_in_one_direction-dia;
-printf("Effective_Depth_of_Footing_in_other_direction = %d mm \n",Effective_Depth_of_Footing_in_other_direction)
-
-elseif(Effective_Depth_on_the_basis_of_Moment<Effective_Depth_on_the_basis_of_One_Way_Shear)
-Effective_Depth_of_the_Footing = Effective_Depth_on_the_basis_of_One_Way_Shear;
-
-Overall_Depth_of_the_Footing = Effective_Depth_of_the_Footing+Clear_Cover_for_Footing;
-printf("Overall_Depth_of_the_Footing = %d mm \n",Overall_Depth_of_the_Footing)
-
-Effective_Depth_of_Footing_in_one_direction = Overall_Depth_of_the_Footing-Clear_Cover;
-printf("Effective_Depth_of_Footing_in_one_direction = %d mm \n",Effective_Depth_of_Footing_in_one_direction)
-
-Effective_Depth_of_Footing_in_other_direction = Effective_Depth_of_Footing_in_one_direction-dia;
-printf("Effective_Depth_of_Footing_in_other_direction = %d mm \n",Effective_Depth_of_Footing_in_other_direction)
-
+% beta_c is the ratio of short side to lonf side of the column so for square column beta_c = 1
+beta_c = 1;
+Ks = (0.5+beta_c);
+if(Ks>1)
+Ks=1;
+elseif(Ks<1)
+Ks=Ks;
 endif
+tc = 0.25*sqrt(Fck);
+depth_two_way_shear = [4*Ks*tc 4*Ks*tc*Side_of_Column -Shear_Force_around_the_section];
+d_two_way_shear = max(roots(depth_two_way_shear));
+printf("Eff_depth_two_way_shear = %d mm \n",d_two_way_shear)
 
 disp("\n")
-% Check for Two Way Shear
-disp("Check for Two Way Shear")
-Width = Side_of_Column*1000+Effective_Depth_of_the_Footing;
-Shear_Force_around_the_section = (Net_Upward_Pressure*(Side_of_Footing*Side_of_Footing-(Width/1000*Width/1000)));
-printf("Shear_Force_around_the_section = %d KN \n",Shear_Force_around_the_section)
-
-Factored_Shear_Force = 1.5*Shear_Force_around_the_section;
-printf("Factored_Shear_Force = %d KN \n",Factored_Shear_Force)
-
-Actual_Shear_Stress = (Factored_Shear_Force*1000)/(4*Width*Effective_Depth_of_the_Footing);
-printf("Actual_Shear_Stress = %d N/mm^2 \n",Actual_Shear_Stress)
-
-Permissible_Shear_Stress2 = 0.25*sqrt(Fck);
-printf("Permissible_Shear_Stress = %d N/mm^2 \n",Permissible_Shear_Stress2)
-
-if(Actual_Shear_Stress<Permissible_Shear_Stress2)
-disp("Hence Safe in Two Way Shear")
-elseif(Actual_Shear_Stress>Permissible_Shear_Stress2)
-disp("Hence not Safe in Two Way Shear")
-endif
+depth = [Effective_Depth_on_the_basis_of_Moment d_one_way_shear d_two_way_shear];
+Eff_depth_of_footing = max(depth);
+printf("Eff_depth_of_footing = %d mm \n",Eff_depth_of_footing)
+Overall_depth_of_footing = round((Eff_depth_of_footing+Clear_Cover+dia)/10)*10;
+printf("Overall_depth_of_footing = %d mm \n",Overall_depth_of_footing)
 
 disp("\n")
 % Design of Reinforcement
-disp("Design of Steel Reinforcement")
-Area_of_steel = 0.5*(Fck/Fy)*(1-sqrt(1-((4.6*Factored_Moment_at_the_Face_of_the_Column)/(Fck*Side_of_Footing*1000*Effective_Depth_of_Footing_in_other_direction*Effective_Depth_of_Footing_in_other_direction))))*Side_of_Footing*1000*Effective_Depth_of_Footing_in_other_direction;
+disp("Design of Steel Reinforcement:-")
+Area_of_steel = 0.5*(Fck/Fy)*(1-sqrt(1-((4.6*M)/(Fck*Side_of_Footing*Eff_depth_of_footing*Eff_depth_of_footing))))*Side_of_Footing*Eff_depth_of_footing;
 printf("Area_of_steel = %d mm^2 \n", Area_of_steel)
 
 Area_of_one_bar = (pi/4)*(dia*dia);                                             
@@ -133,13 +116,11 @@ No_of_Reinforced_Bars = ceil(Area_of_steel/Area_of_one_bar)
 disp("Hence provide these number of Reinforcement Bars uniformuly distributed in each direction.")
 
 disp("\n")
-
-disp("Check for Development Length")
-tbd = interp1 (mat(:,1),mat(:,2),Fck_for_Column);
-
-Development_Length = ceil((Fy)/(4*tbd*1.6))*dia;
+disp("Check for Development Length:-")
+tbd = interp1 (mat(:,1),mat(:,2),Fck);
+Development_Length = round((0.87*Fy)/(4*tbd*1.6))*dia;
 printf("Development_Length = %d mm \n", Development_Length)
-Length_Available_of_Bars = round((0.5*(Side_of_Footing*1000 - Side_of_Column*1000) - (Side_Cover)));
+Length_Available_of_Bars = round((0.5*(Side_of_Footing - Side_of_Column) - (Side_Cover)));
 printf("Length_Available_of_Bars = %d mm \n", Length_Available_of_Bars)
 
 if(Development_Length<Length_Available_of_Bars)
@@ -150,11 +131,10 @@ endif
 
 disp("\n")
 % Check for Transfer of Load at the base
-disp("Check for Transfer of Load at the base")
-A2 = (Side_of_Column*1000)*(Side_of_Column*1000);
+disp("Check for Transfer of Load at the base:-")
+A2 = (Side_of_Column)*(Side_of_Column);
 
-A1 = ((Side_of_Column*1000)+2*(2*Overall_Depth_of_the_Footing))*((Side_of_Column*1000)+2*(2*Overall_Depth_of_the_Footing));
-
+A1 = ((Side_of_Column)+2*(2*Overall_depth_of_footing))*((Side_of_Column)+2*(2*Overall_depth_of_footing));
 
 square_root_A1_by_A2 = sqrt((A1)/(A2));
 if(square_root_A1_by_A2>2)
@@ -165,7 +145,7 @@ endif
 
 Permissible_Bearing_Stress = 0.45*Fck*square_root_A1_by_A2;
 printf("Permissible_Bearing_Stress = %d N/mm^2 \n",Permissible_Bearing_Stress)
-Actual_Bearing_Stress = (1.5*Load_on_Column*1000)/(Side_of_Column*1000*Side_of_Column*1000);
+Actual_Bearing_Stress = (Load_on_Column)/(Side_of_Column*Side_of_Column);
 printf("Actual_Bearing_Stress = %d N/mm^2 \n",Actual_Bearing_Stress)
 
 if(Permissible_Bearing_Stress>Actual_Bearing_Stress)
@@ -173,6 +153,3 @@ disp("Hence Safe in Bearing Stress. Thus no Seperate Dowel Bars are required for
 elseif(Permissible_Bearing_Stress<Actual_Bearing_Stress)
 disp("Hence not Safe in Bearing Stress")
 endif
-
-
-
